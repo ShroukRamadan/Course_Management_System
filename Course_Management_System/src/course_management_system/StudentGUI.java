2fc1ce769307e5ff9cd7eea9ad61309dc680dafd
Esraa Hassouna <58232440+esraahassouna911@users.noreply.github.com>
2019-12-13 23:23:33 +0200

    Add files via upload

---
+++ Course_Management_System/src/course_management_system/StudentGUI.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentgui;
+
+/**
+ *
+ * @author lenovo
+ */
+public class StudentGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           StudentFrame S=new StudentFrame();
+           Admin x=new Admin();
+           CourseDetails cd=new CourseDetails();
+                Payment P =new Payment();
+    
+    
+    }
+    
+}
